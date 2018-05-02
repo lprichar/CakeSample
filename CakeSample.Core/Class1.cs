188bf3241b2bd6f5902ca6984275190a99010f61
Lee Richardson <github@leerichardson.com>
2018-05-01 22:54:42 -0400

    Removing unused code

--- CakeSample.Core/Class1.cs
+++ CakeSample.Core/Class1.cs
@@ -4,23 +4,12 @@ namespace ClassLibrary1
 {
     public class Class1
     {
-        public string Greeting {get; set; }
-
-        public static string SayHello(string name)
+        public static string SayHello(string name) 
         {
             if (string.IsNullOrEmpty(name))
-            {
                 return "Hello Friend";
-            }
             else
-            {
                 return "Hello " + name;
-            }
-        }
-
-        public static string Serialize(Class1 class1)
-        {
-            return Newtonsoft.Json.JsonConvert.SerializeObject(class1);
         }
     }
 }
